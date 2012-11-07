d1ce5a9e3cf592c5e836ce787ccb3a515b13dd4f
Jim <jk@jameskoppen>
2012-11-07 17:26:35 -0600

    Added requirements.txt for use with virtualenv and pip

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ def tweet(irtsi=None,at=None):
 		log(error)
 	else:
 		if irtsi: 
-			status = "in reply to"+irtsi+": "+status
+			status = "In reply to "+irtsi+": "+status
 		log(status)
 
 def reply(mention):
