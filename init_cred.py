4be92eee8dd99c8bf99c8f3a2b10ccb89a3e5c7f
Jim <jk@jameskoppen>
2012-11-07 13:09:03 -0600

    removed init_cred.py from version control

--- init_cred.py
+++
@@ -1,4 +0,0 @@
-from twitter.oauth import write_token_file
-import os
-oauth_filename = os.environ.get('HOME','') + os.sep + '.rc_twitter_oauth'
-write_token_file(oauth_filename,'900564462-7FHP2xeKRYVN0lh3chm3IO7kpiwl4mx4aCpzSHFC','9uku5C5iNOedrJkAh1z59ReaU1CX8EwSwHdnhiTM')
