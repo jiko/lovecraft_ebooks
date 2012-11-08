181f9da03687c6ae5e11e00e3d6822684d25f003
Jim <jk@jameskoppen>
2012-11-07 20:45:04 -0600

    Consolidated corpus; switched markov libraries. Marked improvement in speed and output.

--- bot.py
+++ bot.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python
 import init_twit as tw
-import markov, time, os, re
+import markovgen, time, os, re
 
 # make a separate file for these reusable functions: bot.py
 # main bot-specific app logic in app.py
-# check out jamlitbot setup for best practices
+
+corpus_file = 'corpus.txt'
+with open(corpus_file) as text:
+	markov = markovgen.Markov(text)
+
 def log(msg):
 	with open('log','a') as f:
 		f.write(msg+"\n")
 	print msg
 
 def genTweet():
-	sentence = markov.genSentence(markovLength)
-	while (len (sentence) > 123 or len(sentence) < 20):
-		sentence = markov.genSentence(markovLength)
-	return sentence
+	return markov.generate_markov_text(size=18)
 
-def tweet(irtsi=None,at=None): 
-	status = genTweet()
+def tweet(status,irtsi=None,at=None): 
 	try:
 		if at and irtsi:
 			status = "@"+at+" "+status
-			tw.poster.statuses.update(status=status,in_reply_to_status_id=irtsi)
+			#tw.poster.statuses.update(status=status,in_reply_to_status_id=irtsi)
 		else:
 			pass
-			tw.poster.statuses.update(status=status)
+			#tw.poster.statuses.update(status=status)
 	except tw.poster.TwitterHTTPError as error:
 		log(error)
 	else:
@@ -32,23 +32,15 @@ def tweet(irtsi=None,at=None):
 			status = "In reply to "+irtsi+": "+status
 		log(status)
 
-def reply(mention):
+def reply(txt,mention):
 	asker = mention['from_user']
 	log(asker + " said " + mention['text'])
 	status_id = str(mention['id'])
 	if tw.last_id_replied < status_id:
 		tw.last_id_replied = status_id
-	tweet(status_id,asker)
-
-markovLength = 2
-
-corpus_files = []
-[corpus_files.append("corpus/"+files) for files in os.listdir("corpus") if files.endswith(".txt")]
-
-if (markov.mapping=={}):
-	corpus = []
-	[corpus.extend(markov.wordlist(filename)) for filename in corpus_files]
-	markov.buildMapping(corpus,markovLength)
+	while len(txt) > 123:
+		txt = genTweet()
+	tweet(txt,status_id,asker)
 
 while True:
 	results = tw.twitter.search(q="@"+tw.handle,since_id=tw.last_id_replied)['results']
@@ -57,7 +49,7 @@ while True:
 	if not results:
 		log("Nobody's talking to me...")
 	else:
-		[reply(result) for result in results] 
-	tweet()
+		[reply(genTweet(),result) for result in results] 
+	tweet(genTweet())
 	log("Sweet Dreams...")
 	time.sleep(7600) # waits for two hours
