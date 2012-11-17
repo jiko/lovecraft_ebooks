272b02383fabed87b27b1ffca91d1ccc3dde0484
Jim <jk@jameskoppen>
2012-11-17 14:04:14 -0600

    Changed markov code to do 3-grams instead of 2-grams

--- markovgen.py
+++ markovgen.py
@@ -17,33 +17,32 @@ class Markov(object):
 		return words
 		
 	
-	def triples(self):
-		""" Generates triples from the given data string. So if our string were
-				"What a lovely day", we'd generate (What, a, lovely) and then
-				(a, lovely, day).
+	def quadruples(self):
+		""" 
+Generates quadruples from the given data string. So if our string were "What a very lovely day", we'd generate (What, a, very, lovely) and then (a, very, lovely, day).
 		"""
 		
-		if len(self.words) < 3:
+		if len(self.words) < 4:
 			return
 		
-		for i in range(len(self.words) - 2):
-			yield (self.words[i], self.words[i+1], self.words[i+2])
+		for i in range(len(self.words) - 3):
+			yield (self.words[i], self.words[i+1], self.words[i+2], self.words[i+3])
 			
 	def database(self):
-		for w1, w2, w3 in self.triples():
-			key = (w1, w2)
+		for w1, w2, w3, w4 in self.quadruples():
+			key = (w1, w2, w3)
 			if key in self.cache:
-				self.cache[key].append(w3)
+				self.cache[key].append(w4)
 			else:
-				self.cache[key] = [w3]
+				self.cache[key] = [w4]
 				
 	def generate_markov_text(self, size=25):
-		seed = random.randint(0, self.word_size-3)
-		seed_word, next_word = self.words[seed], self.words[seed+1]
-		w1, w2 = seed_word, next_word
+		seed = random.randint(0, self.word_size-4)
+		seed_word, next_word, last_word = self.words[seed], self.words[seed+1], self.words[seed+2]
+		w1, w2, w3 = seed_word, next_word, last_word
 		gen_words = []
 		for i in xrange(size):
 			gen_words.append(w1)
-			w1, w2 = w2, random.choice(self.cache[(w1, w2)])
-		gen_words.append(w2)
+			w1, w2, w3 = w2, w3, random.choice(self.cache[(w1, w2, w3)])
+		gen_words.append(w3)
 		return ' '.join(gen_words)
