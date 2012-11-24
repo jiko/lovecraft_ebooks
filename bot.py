a911317257e12650a75bfb61734c8fefa73f9021
Jim <jk@jameskoppen>
2012-11-24 14:38:33 -0600

    For a project like this, logging is more trouble than it is worth.

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import init_twit as tw
-import markovgen, time, re, random, codecs
+import markovgen, time, re, random 
 
 # make a separate file for these reusable functions: bot.py
 # main bot-specific app logic in app.py
@@ -8,11 +8,6 @@ import markovgen, time, re, random, codecs
 corpus_file = 'corpus.txt'
 with open(corpus_file) as text:
 	markov = markovgen.Markov(text)
-
-def log(msg):
-	print msg
-	with codecs.open('log','a','utf-8') as f:
-		f.write(msg+"\n")
 
 def genTweet():
 	wc = random.randint(6,18)
@@ -26,16 +21,16 @@ def tweet(status,irtsi=None,at=None):
 		else:
 			pass
 			tw.poster.statuses.update(status=status)
-	except tw.TwitterError as error:
-		log(error.response_data)
+	except tw.TwitterError:
+		print "Twitter Error"
 	else:
 		if irtsi: 
 			status = "In reply to "+irtsi+": "+status
-		log(status)
+		print status
 
 def reply(txt,mention):
 	asker = mention['from_user']
-	log(asker + " said " + mention['text'])
+	print asker + " said " + mention['text']
 	status_id = str(mention['id'])
 	if tw.last_id_replied < status_id:
 		tw.last_id_replied = status_id
@@ -49,9 +44,9 @@ while True:
 	#retweets = re.compile('rt\s',flags=re.I)
 	#results = [response for response in results if not retweets.search(response['text'])]
 	if not results:
-		log("Nobody's talking to me...")
+		print "Nobody's talking to me..."
 	else:
 		[reply(genTweet(),result) for result in results] 
 	tweet(genTweet())
-	log("Sweet Dreams...")
+	print "Sweet Dreams..."
 	time.sleep(7600) # waits for two hours
